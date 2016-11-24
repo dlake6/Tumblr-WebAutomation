4f8b78e863ca36f708d58055f8878692be73fd72
dlake6 <dlake@spartaglobal.co>
2016-11-24 23:21:47 +0000

    invalid credential tests and delete post test

--- tumblr/spec/spec_helper.rb
+++ tumblr/spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require "watir"
+require "rspec"
+
+#@user_name = 'sdet-hero'
 
 
 def logged_in?
@@ -11,24 +14,53 @@ def login
   sleep 1
   @browser.button(id: "login-signin").click
   sleep 1
-  @browser.text_field(id: "login-passwd").set "Acad3my1\n"
+  @browser.text_field(id: "login-passwd").send_keys "Acad3my1\n"
 end 
 
 def logout
   @browser.goto "#{@url}/logout"
 end
 
+def make_post
+  sleep 1
+  @browser.i(class: "icon_post_text").click
+  sleep 1
+  @browser.div(class: "editor-plaintext").send_keys "Test"
+  @browser.div(class: "editor-richtext").send_keys "this is a test"
+  @browser.button(class: "button-area create_post_button").click
+  sleep 1
+  @browser.goto "https://www.tumblr.com/blog/sdet-hero"
+  sleep 1
+  return @browser.lis(class: "post_container")[1].attribute_value("data-pageable")
+end
 
+def delete_post
+  sleep 1
+  @browser.goto "https://www.tumblr.com/blog/sdet-hero"
+  Watir::Wait.until { @browser.lis(class: "post_container")[0].present? }
+  @browser.ol(id: 'posts').lis[1].div(class: 'creator').click
+  Watir::Wait.until { @browser.div(class: 'post_controls_inner').div(class: 'active').present? }
+  @browser.ol(id: 'posts').ul(class: 'popover_inner').lis[1].click
+  sleep 1
+  @browser.button(class: 'blue').click
+end
 
+def find_post
+  if @id == @browser.lis(class: "post_container")[1].attribute_value("data-pageable")
+    return true
+  else
+    return false
+  end
+end
 
-
-
+RSpec.configure do |c|
+  c.filter_run :focus => true
+end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
--- tumblr/spec/tumblr_spec.rb
+++ tumblr/spec/tumblr_spec.rb
@@ -1,7 +1,7 @@
 require 'watir'
 
 
-describe "Tumblr" do 
+describe "Tumblr", :focus do 
 
 	before(:all) do
 		@browser = Watir::Browser.new :chrome
@@ -12,7 +12,7 @@ describe "Tumblr" do
 		if logged_in?
 			@browser.goto "#{@url}/dashboard"
 		else
-			login()
+			login
 		end
 	end
 
@@ -21,18 +21,49 @@ describe "Tumblr" do
 		expect(@browser.url).to include "dashboard"
 	end
 
+  it 'should fail to log in with invalid email' do
+    logout
+    expect(@browser.url).to eq "#{@url}/login"
+    @browser.input(id: 'signup_determine_email').send_keys "hello\n"
+    Watir::Wait.until { @browser.li(class: 'error').visible? }
+    expect(@browser.li(class: 'error').text).to eq 'That\'s not a valid email address. Please try again.'
+    5.times { @browser.input(id: 'signup_determine_email').send_keys :backspace }
+  end
+
+
+
+  it 'should fail to log in with invalid password' do
+    logout
+    expect(@browser.url).to eq "#{@url}/login"
+
+   
+  	@browser.text_field(id:"signup_determine_email").send_keys "dlake@spartaglobal.co\n"
+  	sleep 2
+
+  	@browser.button(id: "login-signin").click
+  	sleep 1
+  	@browser.text_field(id: "login-passwd").send_keys "hello\n"
+
+    Watir::Wait.until { @browser.div(id: 'mbr-login-error').visible? }
+    expect(@browser.div(id: 'mbr-login-error').text).to eq 'Invalid password. Please try again.'
+  end
+
+	
+
 	it "should be able to make a new post" do
-		@browser.i(class: "icon_post_text").click
-		sleep 1
-		@browser.div(class: "editor-plaintext").send_keys "Test"
-		@browser.div(class: "editor-richtext").send_keys "hello there"
-		@browser.button(class: "button-area create_post_button").click
+		make_post
 		sleep 2
 		@browser.goto "https://www.tumblr.com/blog/sdet-hero"
 		sleep 1
 		expect((@browser.divs(class: "post_title")[0]).text).to include "Test"
+		expect((@browser.divs(class: "post_body")[0]).text).to include "this is a test"
 	end
 
-
+	it 'should delete posts' do
+		@id = make_post
+		delete_post
+		sleep 1
+		expect(find_post).to eq false
+	end
 
 end
