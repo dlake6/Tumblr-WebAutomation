66ef5d4634499440c5c716700e64d5328b9a0277
dlake6 <dlake@spartaglobal.co>
2016-11-24 13:20:20 +0000

    login, logout, and logged_in? helper methods

--- tumblr/spec/tumblr_spec.rb
+++ tumblr/spec/tumblr_spec.rb
@@ -3,30 +3,36 @@ require 'watir'
 
 describe "Tumblr" do 
 
-	browser = Watir::Browser.new :chrome
-	url = "https://www.tumblr.com"
+	before(:all) do
+		@browser = Watir::Browser.new :chrome
+		@url = "https://www.tumblr.com"
+	end
+
+	before(:each) do
+		if logged_in?
+			@browser.goto "#{@url}/dashboard"
+		else
+			login()
+		end
+	end
 
 	it 'should login in with valid credentials' do
-		browser.goto "#{url}/login"
-		(browser.text_field(id:"signup_determine_email")).send_keys "dlake@spartaglobal.co\n"
 		sleep 1
-		browser.button(id: "login-signin").click
-		sleep 1
-		browser.text_field(id: "login-passwd").set "Acad3my1\n"
-		sleep 1
-		expect(browser.url).to include "dashboard"
+		expect(@browser.url).to include "dashboard"
 	end
 
 	it "should be able to make a new post" do
-		(browser.i(class: "icon_post_text")).click
+		@browser.i(class: "icon_post_text").click
 		sleep 1
-		(browser.div(class: "editor-plaintext")).send_keys "Test"
-		(browser.div(class: "editor-richtext")).send_keys "hello there"
-		(browser.button(class: "button-area create_post_button")).click
+		@browser.div(class: "editor-plaintext").send_keys "Test"
+		@browser.div(class: "editor-richtext").send_keys "hello there"
+		@browser.button(class: "button-area create_post_button").click
 		sleep 2
-		browser.goto "https://www.tumblr.com/blog/sdet-hero"
+		@browser.goto "https://www.tumblr.com/blog/sdet-hero"
 		sleep 1
-		expect((browser.divs(class: "post_title")[0]).text).to include "Test"
+		expect((@browser.divs(class: "post_title")[0]).text).to include "Test"
 	end
+
+
 
 end
